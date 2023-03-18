ceaa2ed0fc27dddb896c30336516e04343516a74
SamA1430 <samuel.arg14@gmail.com>
2023-03-18 16:23:10 -0400

    update Client_simple.java and Client_simpleLauncher.java

--- src/main/java/client/Client_simple.java
+++ src/main/java/client/Client_simple.java
@@ -1,4 +1,24 @@
 package client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client_simple {
+    private Socket clientSocket;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+
+    public Client_simple(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+    }
+
+    public void run(){
+        try{
+            objectOutputStream = (ObjectOutputStream) clientSocket.getOutputStream();
+            objectInputStream = (ObjectInputStream) clientSocket.getInputStream();
+        } catch (IOException e) {
+
+        }
+    }
+
 }
--- src/main/java/client/Client_simpleLauncher.java
+++ src/main/java/client/Client_simpleLauncher.java
@@ -1,4 +1,17 @@
 package client;
 
+import java.io.IOException;
+
 public class Client_simpleLauncher {
+
+    private static final String IP = "127.0.0.1";
+    private static final int PORT = 1337;
+    public static void main(String[] args) {
+        Client_simple client;
+        try{
+            client = new Client_simple(IP, PORT);
+            client.run();
+        } catch (IOException e) {
+        }
+    }
 }
