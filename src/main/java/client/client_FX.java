c6106c5fafbff767450964a5943dbd9c90a9b45e
SamA1430 <samuel.arg14@gmail.com>
2023-03-30 10:37:56 -0400

    create client_FX.java

---
+++ src/main/java/client/client_FX.java
@@ -0,0 +1,15 @@
+package client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class client_FX extends Application {
+    public static void main(String[] args){
+        client_FX.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+}
