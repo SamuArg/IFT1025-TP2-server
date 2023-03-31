ffc11fca75e9efe129b071130bcd27c85ac2898c
SamA1430 <samuel.arg14@gmail.com>
2023-03-30 21:31:51 -0400

    add tableau liste de cours to Client_fx.java

--- src/main/java/client/Client_fx.java
+++ src/main/java/client/Client_fx.java
@@ -5,12 +5,16 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import server.models.Course;
 
 public class Client_fx extends Application {
     public static void main(String[] args){
@@ -37,15 +41,29 @@ public class Client_fx extends Application {
         rightTitle.setTextAlignment(TextAlignment.CENTER);
         rightTitle.setWrappingWidth(425);
 
-
         root.getChildren().add(leftBox);
         root.setSpacing(20);
+        leftBox.setSpacing(20);
+        rightBox.setSpacing(20);
         root.getChildren().add(rightBox);
 
         leftBox.getChildren().add(leftTitle);
         leftBox.setAlignment(Pos.TOP_LEFT);
         rightBox.getChildren().add(rightTitle);
         rightBox.setAlignment(Pos.TOP_RIGHT);
+
+        TableView tableau = new TableView();
+        TableColumn<Course, String> column1 = new TableColumn<>("Code");
+        column1.setCellValueFactory(new PropertyValueFactory<>("code"));
+        TableColumn<Course, String> column2 = new TableColumn<>("Cours");
+        column2.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableau.getColumns().add(column1);
+        tableau.getColumns().add(column2);
+        tableau.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        leftBox.getChildren().add(tableau);
+
+
 
 
         primaryStage.setTitle("Inscription UdeM");
