1a6fcfb03fe38c0bb4777b9df7c11b2874c64f6b
SamA1430 <117423618+SamA1430@users.noreply.github.com>
2023-04-08 23:21:22 -0400

    add javadoc to Modele.java

--- src/main/java/client/client_fx/Modele.java
+++ src/main/java/client/client_fx/Modele.java
@@ -11,12 +11,22 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Cette classe représente le modèle du client. Elle contient toutes les fonctions qui sont utilent pour représenter la logique du client
+ */
 public class Modele {
     private Socket clientSocket;
     private ObjectOutputStream objectOutputStream;
     private ObjectInputStream objectInputStream;
     private ArrayList<Course> courses;
 
+    /**
+     * Cette méthode permet de charger les cours d'une certaine session via le server
+     * @param session La session pour laquelle on demande la liste de cours
+     * @return Retourne une ArrayList des cours
+     * @throws IOException Si il y a une erreur lors de l'envoie ou de la réception de la requête vers le server
+     * @throws ClassNotFoundException Si la classe Course n'est pas trouvé lors de la lecture de l'input du server
+     */
     public ArrayList<Course> loadCourse(String session) throws IOException, ClassNotFoundException {
         connect();
         objectOutputStream.writeObject("CHARGER "+session);
@@ -24,11 +34,23 @@ public class Modele {
         return courses;
     }
 
+    /**
+     * Cette méthode permet au client de se connecter au server
+     * @throws IOException Si il y a une erreur lors de la création de la connection entre le client et le serveur
+     */
     public void connect() throws IOException {
         clientSocket = new Socket("127.0.0.1",1337);
         objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
         objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
         }
+
+    /**
+     * Cette méthode permet d'envoyer un RegistrationForm du client vers le serveur pour s'inscrire à un cours
+     * @param form Le RegistrationForm à envoyé au server
+     * @return Retourne un message de disant si l'inscription est réussi ou non
+     * @throws IOException Si il y a une erreur entre le client et le server
+     * @throws ClassNotFoundException Si la classe RegistrationForm n'est pas trouvé
+     */
     public String sendForm(RegistrationForm form) throws IOException, ClassNotFoundException {
         connect();
         objectOutputStream.writeObject("INSCRIRE");
@@ -37,16 +59,32 @@ public class Modele {
         return success;
     }
 
+    /**
+     * Cette méthode permet de vérifier si une String respecte le format d'un email valide
+     * @param input La String qu'il faut vérifier le format
+     * @return Retourne true si la String respecte le format, false sinon
+     */
     public boolean isEmail(String input){
         Pattern emailRegex = Pattern.compile("\\S+@\\S+\\.\\S+");
         Matcher matcher = emailRegex.matcher(input);
         return matcher.find();
     }
+
+    /**
+     * Cette méthode permet de vérifier si une String respecte le format d'un nom
+     * @param input La String qu'il faut vérifier le format
+     * @return Retourne true si la String respecte le format, false sinon
+     */
     public boolean isName(String input){
         Pattern nameRegex = Pattern.compile("\\S+");
         Matcher matcher = nameRegex.matcher(input);
         return matcher.find();
     }
+    /**
+     * Cette méthode permet de vérifier si une String respecte le format d'un matricule
+     * @param input La String qu'il faut vérifier le format
+     * @return Retourne true si la String respecte le format, false sinon
+     */
     public boolean isMatricule(String input){
         Pattern matriculeRegex = Pattern.compile("[0-9]{8}");
         Matcher matcher = matriculeRegex.matcher(input);
